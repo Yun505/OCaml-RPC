383976b7966261dca498fa15390f6dadceb4ea67
Hayun Jung <yunjun505@gmail.com>
2024-01-05 23:00:28 -0500

    2.2

---
+++ project/bin/hello-world/hello.ml
@@ -0,0 +1,5 @@
+let _ = print_endline "hello world!"
+(*let _ = means that the name of var does not matter *)
+(* ocamlc -o hello.byte hello.ml -> To save *)
+(* ./hello.byte -> to run  *)
+(* dune exec ./hello.exe -> To run and build a program in one step *)
